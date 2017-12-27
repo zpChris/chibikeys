45610b936d1bceaf7898ec002dbbec8f3cb6a45a
Chris Elliott <christopher7elliott@gmail.com>
2017-12-26 21:19:40 -0800

    testing on phone

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => res.render('index.html'));
+router.get('/keycaps', (req, res, next) => res.render('keycaps.html'));
+router.get('/our-commitment', (req, res, next) => res.render('our-commitment.html'));
 
 module.exports = router;
