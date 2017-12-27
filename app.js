3a1b405f26a38deada88e02af914353ec76ef6bd
Chris Elliott <christopher7elliott@gmail.com>
2017-12-27 12:16:55 -0800

    added 'our commitment' page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const index = require('./routes/index');
+const keycaps = require('./routes/keycaps');
+const our_commitment = require('./routes/our-commitment');
 
 const app = express();
 const port = (process.env.PORT || 3000);
@@ -13,6 +15,8 @@ app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
 
 app.use(index);
+app.use(keycaps);
+app.use(our_commitment);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log('Example app listening on port 3000!'));
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => res.render('index.html'));
-router.get('/our-commitment', (req, res, next) => res.render('our-commitment.html'));
 
 module.exports = router;
